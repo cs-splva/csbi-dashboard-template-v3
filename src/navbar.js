e73cecab814888c77d241637c6a16a78f6273296
Luling, Paul van (SPLVA) - KLM <paul-van.luling@klm.com>
2024-12-12 14:02:38 +0100

    initial upload of all files

---
+++ src/navbar.js
@@ -0,0 +1,39 @@
+var $ = window.CustomJQuery
+
+var elements = Array.from(document.getElementsByClassName("sfx_page-tab_204"));
+var navBar = document.getElementById('js-nav-bar');
+
+function changePage(page) {
+	targetInnerText = "_" + page; // "_" is added because createNewLinks only looks for pages that start with "_"
+  
+	for (let i = 0; i < elements.length; i++) {
+	  const currentElement = elements[i];
+  
+	  if (currentElement.innerText === targetInnerText) {
+		currentElement.click();
+	  }
+	}
+  }
+
+function createNewLinks() {
+  elements.forEach(div => {
+    if (div.innerText.startsWith('_')) { // Check if the innerText starts with "_"
+      const newLinkContainer = document.createElement('div');
+      if (div.classList.contains('sf-element-active-page-tab')) {
+        newLinkContainer.className = 'active';
+      } else {
+        newLinkContainer.className = 'link';
+      }
+      const newLink = document.createElement('a');
+      newLink.textContent = div.innerText.substring(1); // Remove the first character
+      newLink.href = '#'; // Add the appropriate URL for the link
+      newLink.onclick = function() {
+        changePage(div.innerText.substring(1)); // Remove the first character
+      };
+      newLinkContainer.appendChild(newLink);
+      navBar.appendChild(newLinkContainer);
+    }
+  });
+}
+
+createNewLinks();
---
+++ src/toggleNativeNavigation.js
@@ -0,0 +1,45 @@
+var $ = window.CustomJQuery
+
+function showPageNavigation(show) {
+	var divs = document.getElementsByClassName("sfx_navigation-bar_210");
+	var displayValue = show ? "" : "none";
+	for (var i = 0; i < divs.length; i++) {
+		var div = divs[i];
+		div.style.display = displayValue;
+	}
+}
+
+function monitorDivTextChange() {
+    const targetDiv = document.querySelector('.sfx_label_936');
+
+    if (!targetDiv) {
+        console.error('The div with class "sfx_label_936" does not exist.');
+        return;
+    }
+
+    const handleTextChange = (newText) => {
+        if (newText === 'Viewing') {
+            showPageNavigation(false);
+        } else if (newText === 'Editing') {
+            showPageNavigation(true);
+        }
+    };
+
+    const observeTextChanges = () => {
+        const observer = new MutationObserver(mutationsList => {
+            mutationsList.forEach(mutation => {
+                if (mutation.type === 'childList') {
+                    const newText = mutation.target.textContent.trim();
+                    handleTextChange(newText);
+                }
+            });
+        });
+
+        observer.observe(targetDiv, { childList: true, subtree: true });
+    };
+
+    observeTextChanges();
+    handleTextChange(targetDiv.textContent.trim());
+}
+
+monitorDivTextChange();
